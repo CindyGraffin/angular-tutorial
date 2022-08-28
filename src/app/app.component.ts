38a1a5f7669c16b4cef54bd1f679ca2470fe0341
CindyGraffin <graffincindy@gmail.com>
2022-08-28 17:11:02 +0200

    tuto(components): done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "./pokemon";
+import { POKEMONS } from "./mock-pokemon-list";
 
+// Vue = portion de l'écran controllée par un composant, va être définie dans le template d'un composant et pilotée par la classe du composant
+
+// @ est un décorateur
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+	// nom du composant, de la balise
+	selector: "app-root",
+	// définit le code HTML associé à ce composant
+	template: `<h1>Liste de Pokémons</h1> `,
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+
+// OnInit est une interface du cycle de vie du composant, il faut ensuite définir la méthode associée
+export class AppComponent implements OnInit {
+	// pokemons est une propriété, angular va pousser cette valeur dans le template ci-dessus
+	pokemonList: Pokemon[] = POKEMONS;
+
+	// constructor() {
+	// 	this.pokemonList = []; ❌ Il est recommandé de garder toute la logique du composant en dehors du constructeur, qui n'est pas fait pour ça
+	// }
+
+	ngOnInit() {
+		console.table(this.pokemonList);
+		this.selectPokemon(POKEMONS[0])
+	}
+
+	// pokemon: Pokemon est un contrat d'interface, il prend en paramétre un objet métier
+	selectPokemon(pokemon: Pokemon) {
+		console.log(` Vous avez cliqué sur le pokemon ${pokemon.name}`);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+// vient du coeur du framework, utilisé pour déclarer des composants
 import { NgModule } from '@angular/core';
+// module qui va fournir les éléments essentiels au fonctionnement de l'application, par ex ngIf
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // éléments dont a besoin ce module pour fonctionner
   declarations: [
     AppComponent
   ],
+  // éléments qu'on a besoin d'importer dans notre module mais qui sont d'autres modules
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // permet d'utiliser le système d'injection de dépendances d'Angular
   providers: [],
+  // propre au module racine, permet de dire à Angular quel est le premier composant à démarrer
   bootstrap: [AppComponent]
 })
 export class AppModule { }
